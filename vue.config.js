55311e94d987d807738564d39d77ff9f7dcc318a
Robert Boros <robbie13.boros@gmail.com>
2021-01-08 22:59:34 +0100

    add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            '^/api': {
-                target: 'localhost:8000',
-                ws: true,
-                changeOrigin: true
-            },
-        }
+  "devServer": {
+    "proxy": {
+      "^/api": {
+        "target": "localhost:8000",
+        "ws": true,
+        "changeOrigin": true
+      }
     }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
